dcd1576650aafcaef255ab4322be08e8bad03710
cristian <cris.moralesf@duocuc.cl>
2026-08-26 19:25:44 -0400

    se finaliza el programa

---
+++ num_mayor.py
@@ -0,0 +1,18 @@
+#numero mayor
+num1 = int(input("Ingrese un numero entero: "))
+num2 = int(input("Ingrese un numero entero: "))
+num3 = int(input("Ingrese un numero entero: "))
+
+if num1 > num2 and num1 > num2:
+    print(f"el numero {num1} es mayor")
+elif num2 > num1 and num2 > num3:
+    print(f"El numero {num2} es mayor")
+elif num3 > num1 and num3 >num2:
+    print(f"El numero {num3} es mayor")
+
+if num1 < num2 and num1 < num3:
+    print(f"El numer {num1} es menor")
+elif num2 < num1 and num2 < num3:
+    print(f"El numero {num2} es menor")
+elif num3 < num1 and num3 < num2:
+    print(f"El numero {num3} es menor") 
