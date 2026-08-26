c2622b818326ca6adc4d1de0a4d3381d3ea2674e
cristian <cris.moralesf@duocuc.cl>
2026-08-26 18:54:40 -0400

    el programa se finaliza

--- promedio.py
+++ promedio.py
@@ -1,4 +1,9 @@
 nota1 = float(input("Ingrese la primera nota: "))
 nota2 = float(input("Ingrese la segunda nota: "))
 nota3 = float(input("Ingrese la tercera nota: "))
-promedio_presentacion_final = (nota1 *)
+promedio_presentacion_final = (nota1 * 0.3) (nota2 * 0.4) (nota3 * 0.3)
+print(f"El promedio final es: {promedio_presentacion_final:.1f}")
+
+nota_Examen = float((input"Ingrese la nota de examen : "))
+promedio_Final = (nota_Examen * 0.4) + (promedio_presentacion_final * 0.6)
+print(f"El promedio final es: {promedio_Final}")
