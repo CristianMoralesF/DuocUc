2d1aa7f12168ef802857ef49748e6bf0ab5fa56e
cristian <cris.moralesf@duocuc.cl>
2026-08-26 18:47:25 -0400

    se solicita ingresar las notas para ser calculadas

---
+++ promedio.py
@@ -0,0 +1,4 @@
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
+promedio_presentacion_final = (nota1 *)
