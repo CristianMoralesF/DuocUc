074db100dd6437f2774d96e30b91ca3da25c7eb9
cristian <cris.moralesf@duocuc.cl>
2026-08-26 19:12:36 -0400

    se termina el ejercicio, numero de la suerte

--- azar.py
+++ azar.py
@@ -3,3 +3,15 @@ import random
 inferior = int(input("Ingrese un limite inferior: "))
 superior = int(input("Ingrese un limite superior: "))
 
+if inferior > superior :
+    inferior,superior = superior,inferior
+    print("Se han invertido los valores para poder jugar ")
+
+numero_suerte = random.randint(inferior,superior)
+print(numero_suerte)
+
+intento = int(input(f"Tienes un intento, adivina el numero de la suerte entre {inferior} y {superior}: "))
+if intento == numero_suerte:
+    print("Felicidades has acertado!! ")
+else:
+    print(f"No acertaste :C, el numero de la suerte era : {numero_suerte}.")
